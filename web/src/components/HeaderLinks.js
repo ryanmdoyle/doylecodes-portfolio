3dc4a6a26c864233ddbd2bbfe17b027651cf6726
Ryan <ryandoyle@me.com>
2019-07-06 14:40:05 -0700

    add component for header links (from old site)

---
+++ web/src/components/HeaderLinks.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeaderLinks = () => {
+  return (
+    <div>
+      <a href="https://github.com/ryanmdoyle" target="_blank" rel="noopener noreferrer" alt="GitHub"><i class="fab fa-github"></i></a> <a href="https://dev.to/ryanmdoyle" target="_blank" rel="noopener noreferrer" alt="Dev.to"><i class="fab fa-dev"></i></a> <a href="https://www.freecodecamp.org/ryanmdoyle" target="_blank" rel="noopener noreferrer" alt="FreeCodeCamp"><i class="fab fa-free-code-camp"></i></a> <a href="https://codepen.io/ryanmdoyle/" target="_blank" rel="noopener noreferrer" alt="Codepen"><i class="fab fa-codepen"></i></a> <a href="https://www.twitter.com/doylecodes" target="_blank" rel="noopener noreferrer" alt="Twitter"><i class="fab fa-twitter"></i></a>
+    </div>
+  );
+};
+
+export default HeaderLinks;
