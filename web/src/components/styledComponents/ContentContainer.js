ea975184ff259541f9a1d32aaffeae1bbb37d708
Ryan <ryandoyle@me.com>
2019-07-10 14:40:54 -0700

    create component to pace content on (over the background)

---
+++ web/src/components/styledComponents/ContentContainer.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const ContentContainer = styled.div`
+  margin: 0;
+  padding: 2rem;
+  width: 100%;
+  height: 90%;
+  min-height: 500px;
+  border-radius: 0.25rem;
+  background-color: #cbced3;
+  box-shadow: 0 0 5px rgb(0, 0, 0, 0.1);
+  opacity: 0.9;
+`;
+
+export default ContentContainer;
