778a39d2dc456f0d540c17deac33108ce29b75d3
Ryan <ryandoyle@me.com>
2019-07-11 17:39:37 -0700

    add about placeholder text

--- web/src/pages/about.js
+++ web/src/pages/about.js
@@ -14,6 +14,8 @@ const About = () => (
       <AboutImage>
         <Image />
       </AboutImage>
+      <h2>Hello, I'm Ryan.</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In faucibus semper ex in volutpat. Integer interdum nisi in diam faucibus, auctor laoreet arcu tincidunt. Donec mollis volutpat eros a viverra. Aenean eu dui nec nisi egestas suscipit nec eu turpis. Nunc dignissim odio quis elit sollicitudin, nec vehicula augue euismod. Etiam eget nunc id dui eleifend finibus quis quis eros. Curabitur rutrum odio sit amet ultricies blandit. Proin quis aliquet tortor. Morbi id diam dapibus mi condimentum pretium vitae a risus. Duis sit amet ante sed elit bibendum iaculis ornare vitae orci. Morbi ac eros elit. Donec vehicula lacus justo, vel pretium massa molestie eget. Etiam ac dui purus. </p>
     </ContentContainer>
   </Layout>
 )
