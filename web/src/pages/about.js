fa23c1d9058248d59074bdfdc0668537e8bf12d9
Ryan <ryandoyle@me.com>
2019-07-18 15:05:16 -0700

    add alt to img

--- web/src/pages/about.js
+++ web/src/pages/about.js
@@ -12,7 +12,7 @@ const About = ({ data }) => (
     <h1>About Me</h1>
     <ContentContainer>
       <AboutImage>
-        <img src={profile} />
+        <img src={profile} alt='profile' />
       </AboutImage>
       <h2>Hello, I'm Ryan.</h2>
       <p>I grew up in the Bay Area and have lived around Sacramento for the last 15 years (with a short year in Lancaster, CA). I've spend a decade teaching math and science to 6-8th graders. I love to build new applications and learn new technologies to help me improve everything I've made.  I'm constantly looking at what I can do to make things better (My wife would say I'm a type 1 on the enneagram). When I'm not teaching, coding, or writing about what I'm doing I can be found keeping my daughter occupied, going out to eat and get coffee with my wife, hiking, camping, watching soccer, or nerding out over educational documentaries.</p>
