1b2e3a1a5f25169947ea4dfa27ccd520ccbdd1d8
Ryan <ryandoyle@me.com>
2019-07-17 18:01:17 -0700

    add live links to header

--- web/src/components/HeaderLinks.js
+++ web/src/components/HeaderLinks.js
@@ -10,11 +10,46 @@ const HeaderLinks = () => {
   return (
     <div className='icons'>
       <ul>
-        <li><FontAwesomeIcon icon={faGithub} /></li>
-        <li><FontAwesomeIcon icon={faDev} /></li>
-        <li><FontAwesomeIcon icon={faFreeCodeCamp} /></li>
-        <li><FontAwesomeIcon icon={faCodepen} /></li>
-        <li><FontAwesomeIcon icon={faTwitter} /></li>
+        <li>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href='https://github.com/ryanmdoyle'>
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href='https://dev.to/doylecodes'>
+            <FontAwesomeIcon icon={faDev} />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href='https://www.freecodecamp.org/ryanmdoyle'>
+            <FontAwesomeIcon icon={faFreeCodeCamp} />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href='https://codepen.io/ryanmdoyle/#'>
+            <FontAwesomeIcon icon={faCodepen} />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href='https://twitter.com/doylecodes'>
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+        </li>
       </ul>
     </div>
 
