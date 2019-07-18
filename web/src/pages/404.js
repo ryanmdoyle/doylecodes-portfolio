6be58b447fd629ae1da92bda58dcc5662553baad
Ryan <ryandoyle@me.com>
2019-07-18 15:05:03 -0700

    remove unused component

--- web/src/pages/404.js
+++ web/src/pages/404.js
@@ -1,9 +1,7 @@
 import React from "react"
 
-import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 import PageFade from '../components/PageFade';
-
 
 const NotFoundPage = () => (
   <PageFade>
