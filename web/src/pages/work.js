c1b487269d68b9f8e1558ac8edeb990c570d4b03
Ryan <ryandoyle@me.com>
2019-07-17 18:05:27 -0700

    map through array or projects from sanity

--- web/src/pages/work.js
+++ web/src/pages/work.js
@@ -5,6 +5,7 @@ import SEO from "../components/SEO";
 import ContentContainer from '../components/styledComponents/ContentContainer';
 import PageFade from '../components/PageFade';
 import PortfolioProject from '../components/PortfolioProject';
+import project from "../../../studio/schemas/project";
 
 const PortfolioGrid = styled.div`
   display: flex;
@@ -16,36 +17,43 @@ const PortfolioGrid = styled.div`
   margin: 0;
 `;
 
-const Work = () => (
+const Work = ({ data }) => (
   <PageFade>
     <SEO title="Work" />
     <h1>Work</h1>
     <ContentContainer>
       <PortfolioGrid>
-        <PortfolioProject 
-          title={'Portfolio'}
-          description={'React | Styled-Components | Gatsby | Sanity'}
-          href={'http://www.google.com'}
-        />
-        <PortfolioProject 
-          title={'the title'}
-          description={'the description'}
-          href={'http://www.google.com'}
-        />
-        <PortfolioProject 
-          title={'the title'}
-          description={'the description'}
-          href={'http://www.google.com'}
-        />
-        <PortfolioProject 
-          title={'the title'}
-          description={'the description'}
-          href={'http://www.google.com'}
-        />
+        {data.allSanityProject.nodes.map(p => (
+          <PortfolioProject
+            title={p.title}
+            description={p.description}
+            background={p.featureImage.asset.fixed.src}
+            href={'http://www.google.com'}
+          />
+        ))}
       </PortfolioGrid>
 
     </ContentContainer>
   </PageFade>
 )
+
+export const query = graphql`
+  query MyQuery {
+    allSanityProject {
+      nodes {
+        description
+        id
+        title
+        featureImage {
+          asset {
+            fixed {
+              src
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default Work
