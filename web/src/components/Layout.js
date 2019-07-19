57860d0e50fd85d25f7e48558ce5814dd66c48f0
Ryan Doyle <doylecodes@gmail.com>
2019-07-18 17:52:16 -0700

    back to styled-component

--- web/src/components/Layout.js
+++ web/src/components/Layout.js
@@ -1,10 +1,11 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from "react";
+import PropTypes from "prop-types";
 import styled from 'styled-components';
-import { Spring } from 'react-spring/renderprops'
+import { Spring } from 'react-spring/renderprops';
 
-import Header from "./Header"
-import "./layout.css"
+import Header from "./Header";
+import "./layout.css";
+import mountains from '../images/mountains.jpg'
 
 const MainStyled = styled.main`
   margin: 0 auto;
@@ -12,17 +13,19 @@ const MainStyled = styled.main`
   padding: 0 1.1rem 1.5rem 1.1rem;
 `;
 
+const Background = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-image: url(${mountains});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+`;
+
 const Layout = ({ children }) => {
 
   return (
-      <div style={{
-        height: '100vh',
-        width: '100%',
-        backgroundImage: `url(https://res.cloudinary.com/dwut3uz4n/image/upload/v1563496204/mountains.jpg)`,
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        backgroundSize: 'cover'}
-      }>
+      <Background>
         <Header />
         <Spring
           from={{ opacity: 0 }}
@@ -36,7 +39,7 @@ const Layout = ({ children }) => {
             </div>
           )}
         </Spring>
-      </div>
+      </Background>
   )
 }
 
