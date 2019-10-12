e87e0c0fc526f66334da599aeb562bb3834c132d
Ryan Doyle <doylecodes@gmail.com>
2019-10-12 09:27:04 -0700

    remove commented code

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -57,30 +57,6 @@ const NavList = styled.ul`
     border-bottom: 1px solid white;
     position: relative;
   }
-
-  li:before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: yellow;
-    z-index: -1;
-    transform: rotateX('90');
-    /* translate here */
-    transition: transform 0.3s;
-  }
-
-  li:hover, a:hover {
-    transform: scale(1.2);
-    color: rebeccapurple;
-    transition: color 1s, transform 0.5s;
-  }
-  a {
-    color: #fafafa;
-    text-decoration: none;
-  }
 `;
 
 const Header = ({ siteTitle }) => {
@@ -92,38 +68,8 @@ const Header = ({ siteTitle }) => {
           <li><Link to="/work/">Work</Link></li>
           <li><Link to="/about/">About</Link></li>
           <li><Link to="/contact/">Contact</Link></li>
-          <SwipeButton/>
         </NavList>
       </NavContainer>
-      {/* <Location>
-        {({ location }) => {
-          if (location.pathname !== "/") {
-            console.log(location);
-            return (
-              <Spring
-                to={{ opacity: 1 }}>
-                {props => (
-                  <div style={props}>
-                    <HeaderLinks />
-                  </div>
-                )}
-              </Spring>
-            )
-          }
-          if (location.pathname === "/") {
-            return (
-              <Spring
-                to={{ opacity: 0 }}>
-                {props => (
-                  <div style={props}>
-                    <HeaderLinks />
-                  </div>
-                )}
-              </Spring>
-            )
-          }
-        }}
-      </Location> */}
     </HeaderStyled>
   )
 }
