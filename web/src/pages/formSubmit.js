2088fb944ddc526d272272c1433508865aa5ad2a
Ryan Doyle <doylecodes@gmail.com>
2019-10-18 20:48:45 -0700

    redirect fix

---
+++ web/src/pages/formSubmit.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SEO from '../components/SEO'
+
+export default function formSubmit() {
+  return (
+    <div>
+      <SEO title="Thank you!" />
+      <h3>Thank you!</h3>
+      <p>
+        Thanks for getting in touch. You should hear back from me shortly. If you have not already, feel free to check out some of my writing on <a href='https://dev.to/doylecodes' rel="noopener noreferrer" target="_blank">dev.to</a>.
+      </p>
+    </div>
+  )
+}
